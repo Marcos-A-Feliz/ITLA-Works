52c4650e2657c98fbd6ce75c4e9d41a44c103d63
Marcos-A-Feliz <marcosadrianchenfeliz@gmail.com>
2025-10-05 22:00:10 -0400

    Uploading my second HomeWork

---
+++ HomeWork 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+//Marcos Adrian Feliz 2025-0934
+
+class program
+{
+    static void Main ()
+    {
+        bool success = false;
+       
+            WriteLine("Type an int number and I will tell you if is even or odd");
+        do 
+        {    
+            try
+            {
+
+                int num = int.Parse(ReadLine());
+                WriteLine((num % 2 == 0) ? "The number is even " : "The number is odd");
+                success = true;
+            }
+       
+            catch (OverflowException)
+            {
+                WriteLine("The number inserted is way too big, insert a smaller number");
+                success = false;
+            }
+            catch (FormatException){
+                WriteLine("You have inserted something that is not a number, please insert an int number");
+                success= false;
+            }
+            catch (Exception ex)
+            { 
+                WriteLine("An unexpected error has occurred" + ex.Message);
+            }
+        } while (!success);
+    }
+}
